300c62cb681d07a11fc2ed5ded83debc9623b93d
Rodrigo Poblete <108855218+rodrigoDev0n@users.noreply.github.com>
2022-07-27 17:03:04 -0500

    Update fireabase.js

--- src/helpers/fireabase.js
+++ src/helpers/fireabase.js
@@ -4,17 +4,14 @@ import { getFirestore}  from "@firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "",
-  authDomain: "dashboard-cda.firebaseapp.com",
-  projectId: "dashboard-cda",
-  storageBucket: "dashboard-cda.appspot.com",
-  messagingSenderId: "342993087466",
-  appId: "1:342993087466:web:defc392952d96a955ea093"
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 export const app = initializeApp(firebaseConfig);
 
-/* la variable auth sirve para autenticar usuarios con firebase: */
 export const auth = getAuth(app);
-/* la variable db nos permite obtener los datos de nuestra base de datos para cargarlos donde
-creamos conveniente */
 export const db = getFirestore(app);
